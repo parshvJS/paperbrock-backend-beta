ee7adbaa175b8a43cb2d3b53c226fd3dd4ab829c
parshvJS <parshvsheth2006@gmail.com>
2024-02-10 15:20:23 +0530

    g

--- src/app.js
+++ src/app.js
@@ -1,29 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import { whitelist } from './constants.js'
-const app = express()
+import express from 'express';
+import cors from 'cors';
+import { whitelist } from './constants.js';
 
-app.use(
-    cors()
-);
-app.options('/api/v1/beta/register', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.status(200).end();
-});
+const app = express();
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.options('*', cors(corsOptions)); // Enable preflight requests for all routes
+
+app.use(cors(corsOptions));
 
 app.use(express.json({
-    limit: '16kb'
-}))
+  limit: '16kb'
+}));
 
 app.use(express.urlencoded({
-    extended: true,
-    limit: '16kb'
-}))
+  extended: true,
+  limit: '16kb'
+}));
 
-import router from './routes/user.router.js'
+import router from './routes/user.router.js';
 
-app.use('/api/v1/beta', router)
+app.use('/api/v1/beta', router);
+
 export default app;
