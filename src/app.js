f2e7fe343da53b675937b1f2d2a101811cb0e30f
parshvJS <parshvsheth2006@gmail.com>
2024-02-10 14:51:56 +0530

    f

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ app.use(
 );
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://paperbrock.vercel.app/');
-    // Add other CORS headers as needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+        // Add other CORS headers as needed
     next();
   });
 
