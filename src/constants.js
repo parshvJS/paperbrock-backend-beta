9f339f267c9708a8be100ed4235a838b2d94dc5e
parshvJS <parshvsheth2006@gmail.com>
2024-02-10 10:53:32 +0530

    minor bug fixed

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 const DB_NAME = "paperpro2024"
-const whitelist=['https://paperbrock.vercel.app','https://paperbrock.com','http://localhost:3000', 'http://localhost:5173', 'http://localhost:8000']
+const whitelist=['https://paperbrock.vercel.app/','https://paperbrock.com','http://localhost:3000', 'http://localhost:5173', 'http://localhost:8000']
 export {DB_NAME,whitelist}
