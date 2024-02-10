d49863bf31d76297a2fed1da7d68c7c01873ce71
parshvJS <parshvsheth2006@gmail.com>
2024-02-10 11:14:08 +0530

    cors error

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
-import express from 'express'
-import cors from 'cors'
-import { whitelist } from './constants.js'
-const app = express()
+import express from 'express';
+import cors from 'cors';
+import { whitelist } from './constants.js';
+const app = express();
 
-app.use(
- cors({
-    origin:whitelist,
-    credentials: true
- })
-)
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the origin is in the whitelist or if it is undefined (for requests without origin)
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the request
+    }
+  },
+  credentials: true // Include credentials in the CORS request (e.g., cookies, authorization headers)
+};
 
-app.use(express.json({
-    limit:'16kb'
-}))
+// Enable CORS with specific options
+app.use(cors(corsOptions));
 
-app.use(express.urlencoded({
-    extended:true,
-    limit:'16kb'
-}))
+// Parse JSON requests
+app.use(express.json({ limit: '16kb' }));
 
-import router from './routes/user.router.js'
+// Parse URL-encoded requests
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
-app.use('/api/v1/beta',router)
+import router from './routes/user.router.js';
+
+app.use('/api/v1/beta', router);
+
 export default app;
